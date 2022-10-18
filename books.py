5531f84b08e5aeb3a11fe82c578a8d5497cf009b
BL4IS3 <bl4is3@free.fr>
2022-10-18 11:51:35 +0200

    correction de nom de variable et bug

--- books.py
+++ books.py
@@ -3,8 +3,8 @@ import csv
 from bs4 import BeautifulSoup
 
 
-def get_datas_for_one_product(product):
-    product_page_url = url + "catalogue/" + product
+def get_datas_for_one_product(product, category):
+    product_page_url = url_site + "catalogue/" + product
     print(product_page_url)
     page = requests.get(product_page_url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -29,27 +29,19 @@ def get_datas_for_one_product(product):
     else:
         product_description = ''
     print("Description :", product_description)
-    image_url = soup.img['src']
+    image_url = soup.img['src'].replace('../../', url_site)
     print("lien_image :", image_url)
     review_rating = elements[6]
     print("Reviews :", review_rating)
-    liens = soup.select('ul.breadcrumb > li > a')
-    category = liens[2].string
-    print("Category :", category)
-
-    en_tete = ["product_page_url", "universal_product_code", "title", "price_including_tax",
-               "price_excluding_tax", "number_available", "product_description", "category",
-               "review_rating", "image_url"]
     file = category + ".csv"
-    with open(file, "w") as csv_file:
+    with open(file, "a") as csv_file:
         writer = csv.writer(csv_file, delimiter=",")
-        writer.writerow(en_tete)
         writer.writerow([product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available,
                          product_description, category, review_rating, image_url])
 
 
 def get_all_products_for_one_category(category):
-    url_category = url + "catalogue/category/books/" + category
+    url_category = url_site + "catalogue/category/books/" + category
     n = 1
     list_products = []
     while n != 0:
@@ -82,10 +74,14 @@ def get_all_categories(url):
     for category in cats:
         cat = str(category.a["href"].replace('catalogue/category/books/', '').replace("/index.html", ""))
         categories.append(cat)
+        en_tete = ["product_page_url", "universal_product_code", "title", "price_including_tax",
+                   "price_excluding_tax", "number_available", "product_description", "category",
+                   "review_rating", "image_url"]
+        file = cat + ".csv"
+        with open(file, "w") as csv_file:
+            writer = csv.writer(csv_file, delimiter=",")
+            writer.writerow(en_tete)
     return categories
-
-
-url = "http://books.toscrape.com/"
 
 
 def get_all_datas(url):
@@ -93,14 +89,9 @@ def get_all_datas(url):
     for category in categories:
         products = get_all_products_for_one_category(category)
         for product in products:
-            get_datas_for_one_product(product)
+            get_datas_for_one_product(product, category)
 
 
-get_all_datas(url)
-# categories = get_all_categories(url)
-# for category in categories:
-#     get_all_products_for_one_category(category)
-#     products = get_all_products_for_one_category(category)
-#     print(category)
-#     for product in products:
-#         print(product)
+url_site = "http://books.toscrape.com/"
+
+get_all_datas(url_site)
